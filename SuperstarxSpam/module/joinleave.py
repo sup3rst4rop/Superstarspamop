42f50ccf534019ced09e6909db0ffbe58f5ce4c5
superstar <109408511+sup3rst4rop@users.noreply.github.com>
2022-07-17 15:51:25 +0530

    Create joinleave.py

---
+++ SuperstarxSpam/module/joinleave.py
@@ -0,0 +1,59 @@
+
+# Superstar - Telegram Projects
+# (c) 2022 - 2023 Star
+# Don't Kang Bitch -!
+
+
+
+from Superstarxspam import (OWNER_ID, HNDLR, SUDO_USERS, LOGS_CHANNEL)
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["join"], prefixes=HNDLR))
+@Client.on_message(filters.me & filters.command(["join"], prefixes=HNDLR))
+async def join(Superstarxspam: Client, e: Message):
+    chat = e.text[6:]
+    if chat.isnumeric():
+        return await e.reply_text("Can't join a chat with chat id. Give username or invite link.")
+    try:
+      await Superstarxspam.join_chat(chat)
+      await e.reply_text("**Join Successfully ✅ **")
+    except Exception as ex:
+        await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
+    if LOGS_CHANNEL:
+         try:
+             await Superstarxspam.send_message(LOGS_CHANNEL, f"Joined New Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}")
+         except Exception as a:
+             print(a)
+             pass
+
+@Client.on_message(filters.user(SUDO_USERS) & filters.command(["leave"], prefixes=HNDLR))
+@Client.on_message(filters.me & filters.command(["leave"], prefixes=HNDLR))
+async def leave(Superstarxspam: Client, e: Message):
+    rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+    if len(e.text) > 7:
+        chat = Superstarxspam[0]
+        try:
+           await Superstarxspam.leave_chat(chat)
+           await e.reply_text("**Left Successfully ✅ **")
+        except Exception as ex:
+           await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
+    else:
+        chat = e.chat.id
+        ok = e.from_user.id
+        if int(chat) == int(ok):
+            return await e.reply_text(f"Usage: {HNDLR}leave <chat username or id> or {HNDLR}leave (type in Group for Direct leave)")
+        if int(chat) == -1001706953175:
+              return e.reply_text("**Error**")
+        try:
+           await Superstarxspam.leave_chat(chat)
+           await e.reply_text("**Left Successfully ✅ **")
+        except Exception as ex:
+           await e.reply_text(f"**ERROR:** \n\n{str(ex)}")
+        if LOGS_CHANNEL:
+           try:
+                await Superstarxspam.send_message(LOGS_CHANNEL, f"Leaved Chat \n\n Chat: {chat} \n Cmd By User: {e.from_user.id}")
+           except Exception as a:
+             print(a)
+             pass
