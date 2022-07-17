177125763071b7418dc50d44d7ff808482bda516
superstar <109408511+sup3rst4rop@users.noreply.github.com>
2022-07-17 13:49:10 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,98 @@
+# Superstar - Telegram Projects
+# (c) 2022 - 2023 Star
+# Don't Kang Bitch -!
+
+
+
+import asyncio
+from pyrogram import idle
+from . import (SuperstarxSpam, SuperstarxSpam2, SuperstarxSpam3, SuperstarxSpam4, SuperstarxSpam5, 
+                SuperstarxSpam6, SuperstarxSpam7, SuperstarxSpam8, SuperstarxSpam9, SuperstarxSpam10, 
+                SuperstarxSpam11, SuperstarxSpam12, SuperstarxSpam13, SuperstarxSpam14, SuperstarxSpam15, 
+                SuperstarxSpam16, SuperstarxSpam17, SuperstarxSpam18, SuperstarxSpam19, SuperstarxSpam20, hl)
+
+if SuperstarxSpam:
+    SuperstarxSpam.start()
+    SuperstarxSpam.join_chat("tsf_brand")
+    SuperstarxSpam.join_chat("tsf_brand")
+if SuperstarxSpam2:
+    SuperstarxSpam2.start()
+    SuperstarxSpam2.join_chat("tsf_brand")
+    SuperstarxSpam2.join_chat("tsf_brand")
+if SuperstarxSpam3:
+    SuperstarxSpam3.start()
+    SuperstarxSpam3.join_chat("tsf_brand")
+    SuperstarxSpam3.join_chat("tsf_brand")
+if SuperstarxSpam4:
+    SuperstarxSpam4.start()
+    SuperstarxSpam4.join_chat("tsf_brand")
+    SuperstarxSpam4.join_chat("tsf_brand")
+if SuperstarxSpam5:
+    SuperstarxSpam5.start()
+    SuperstarxSpam5.join_chat("tsf_brand")
+    SuperstarxSpam5.join_chat("tsf_brand")
+if SuperstarxSpam6:
+    SuperstarxSpam6.start()
+    SuperstarxSpam6.join_chat("tsf_brand")
+    SuperstarxSpam6.join_chat("tsf_brand")
+if SuperstarxSpam7:
+    SuperstarxSpam7.start()
+    SuperstarxSpam7.join_chat("tsf_brand")
+    SuperstarxSpam7.join_chat("tsf_brand")
+if SuperstarxSpam8:
+    SuperstarxSpam8.start()
+    SuperstarxSpam8.join_chat("tsf_brand")
+    SuperstarxSpam8.join_chat("tsf_brand")
+if SuperstarxSpam9:
+    SuperstarxSpam9.start()
+    SuperstarxSpam9.join_chat("tsf_brand")
+    SuperstarxSpam9.join_chat("tsf_brand")
+if SuperstarxSpam10:
+    SuperstarxSpam10.start()
+    SuperstarxSpam10.join_chat("tsf_brand")
+    SuperstarxSpam10.join_chat("tsf_brand")
+if SuperstarxSpam11:
+    SuperstarxSpam11.start()
+    SuperstarxSpam11.join_chat("tsf_brand")
+    SuperstarxSpam11.join_chat("tsf_brand")
+if SuperstarxSpam12:
+    SuperstarxSpam12.start()
+    SuperstarxSpam12.join_chat("tsf_brand")
+    SuperstarxSpam12.join_chat("tsf_brand")
+if SuperstarxSpam13:
+    SuperstarxSpam13.start()
+    SuperstarxSpam13.join_chat("tsf_brand")
+    SuperstarxSpam13.join_chat("tsf_brand")
+if SuperstarxSpam14:
+    SuperstarxSpam14.start()
+    SuperstarxSpam14.join_chat("tsf_brand")
+    SuperstarxSpam14.join_chat("tsf_brand")
+if SuperstarxSpam15:
+    SuperstarxSpam15.start()
+    SuperstarxSpam15.join_chat("tsf_brand")
+    SuperstarxSpam15.join_chat("tsf_brand")
+if SuperstarxSpam16:
+    SuperstarxSpam16.start()
+    SuperstarxSpam16.join_chat("tsf_brand")
+    SuperstarxSpam16.join_chat("tsf_brand")
+if SuperstarxSpam17:
+    SuperstarxSpam17.start()
+    SuperstarxSpam17.join_chat("tsf_brand")
+    SuperstarxSpam17.join_chat("tsf_brand")
+if SuperstarxSpam18:
+    SuperstarxSpam18.start()
+    SuperstarxSpam18.join_chat("tsf_brand")
+    SuperstarxSpam18.join_chat("tsf_brand")
+if SuperstarxSpam19:
+    SuperstarxSpam19.start()
+    SuperstarxSpam19.join_chat("tsf_brand")
+    SuperstarxSpam19.join_chat("tsf_brand")
+if SuperstarxSpam20:
+    SuperstarxSpam20.start()
+    SuperstarxSpam20.join_chat("tsf_brand")
+    SuperstarxSpam20.join_chat("tsf_brand")
+
+print("Your Spam Bots Successfully Deployed ✅")
+print("Visit -! @tsf_brand")
+
+idle()
